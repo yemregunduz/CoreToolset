16494b997b5e6012bf9678387d29c0a0c27669d0
yunusemregunduz <emre.gunduz@mikro.com.tr>
2023-12-31 03:04:59 +0300

    CryptographyHelperTests added.

---
+++ tests/CoreToolset.HelpersTests/CrypthographyHelperTests.cs
@@ -0,0 +1,47 @@
+﻿using CoreToolset.Helpers;
+using System.Security.Cryptography;
+
+
+namespace CoreToolset.HelpersTests
+{
+    [TestFixture]
+    public class CryptographyHelperTests
+    {
+        [Test]
+        public void EncryptAndDecrypt_ValidPlainText_MatchesOriginal()
+        {
+            string plainText = "This is a test message";
+            CryptographyHelper cryptographyHelper = new();
+            string encryptedText = cryptographyHelper.Encrypt(plainText);
+            string decryptedText = cryptographyHelper.Decrypt(encryptedText);
+            Assert.That(decryptedText, Is.EqualTo(plainText));
+        }
+
+        [Test]
+        public void VerifyEncryptedText_ValidPlainTextAndEncryptedText_ReturnsTrue()
+        {
+            string plainText = "This is a test message";
+            CryptographyHelper cryptographyHelper = new();
+            string encryptedText = cryptographyHelper.Encrypt(plainText);
+            bool result = cryptographyHelper.VerifyEncyrptedText(plainText, encryptedText);
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void VerifyEncryptedText_InvalidPlainText_ReturnsFalse()
+        {
+            string plainText = "This is a test message";
+            CryptographyHelper cryptographyHelper = new();
+            string encryptedText = cryptographyHelper.Encrypt(plainText);
+            bool result = cryptographyHelper.VerifyEncyrptedText("InvalidPlainText", encryptedText);
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void Decrypt_NullEncryptedText_ThrowsException()
+        {
+            CryptographyHelper cryptographyHelper = new();
+            Assert.Throws<ArgumentNullException>(() => cryptographyHelper.Decrypt(null));
+        }
+    }
+}

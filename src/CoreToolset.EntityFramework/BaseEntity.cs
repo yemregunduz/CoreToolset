8bce396bdb32b4587bf647f96274802ca43c302b
yunusemregunduz <emre.gunduz@mikro.com.tr>
2024-01-01 14:53:12 +0300

    Added CoreToolset.EntityFramework project for implementing database operations through pre-built methods using Entity Framework

---
+++ src/CoreToolset.EntityFramework/BaseEntity.cs
@@ -0,0 +1,30 @@
+﻿namespace CoreToolset.EntityFramework
+{
+    public class BaseEntity : AuditLogEntity, ISoftDelete
+    {
+        public int Id { get; set; }
+        public virtual bool IsDeleted { get; set; } = false;
+        public virtual int? DeletedBy { get; set; }
+        public virtual DateTime? DeletedDate { get; set; }
+        public virtual void MarkAsDelete(int userId)
+        {
+            IsDeleted = true;
+            DeletedDate = DateTime.Now;
+            DeletedBy = userId;
+        }
+    }
+    public interface ISoftDelete
+    {
+        public bool IsDeleted { get; set; }
+        public int? DeletedBy { get; set; }
+        public DateTime? DeletedDate { get; set; }
+        void MarkAsDelete(int userId);
+    }
+    public abstract class AuditLogEntity
+    {
+        public virtual int? CreatedBy { get; set; }
+        public virtual DateTime CreatedDate { get; set; }
+        public virtual int? UpdatedBy { get; set; }
+        public virtual DateTime? UpdatedDate { get; set; }
+    }
+}
---
+++ src/CoreToolset.EntityFramework/Repositories/IQuery.cs
@@ -0,0 +1,7 @@
+﻿namespace CoreToolset.EntityFramework.Repositories
+{
+    public interface IQuery<T>
+    {
+        IQueryable<T> Query();
+    }
+}

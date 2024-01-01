a9da223b32b4619d9b1b076a54066e231634d59e
yunusemregunduz <emre.gunduz@mikro.com.tr>
2024-01-01 14:35:10 +0300

    async PeformanceTester added.

--- tests/CoreToolset.ExtensionsTests/Helpers/PerformanceTestHelper.cs
+++ tests/CoreToolset.ExtensionsTests/Helpers/PerformanceTestHelper.cs
@@ -2,7 +2,7 @@
 
 namespace CoreToolset.ExtensionsTests.Helpers
 {
-    internal class PerformanceTestHelper
+    public class PerformanceTestHelper
     {
         public static (long averageTime, long maxTime) RunPerformanceTest(Action testAction, int numberOfRuns)
         {
@@ -17,6 +17,29 @@ namespace CoreToolset.ExtensionsTests.Helpers
             {
                 stopwatch.Restart();
                 testAction.Invoke();
+                long elapsedMilliseconds = stopwatch.ElapsedMilliseconds;
+                elapsedMillisecondsList.Add(elapsedMilliseconds);
+            }
+
+            long averageTime = (long)elapsedMillisecondsList.Average();
+            long maxTime = elapsedMillisecondsList.Max();
+
+            return (averageTime, maxTime);
+        }
+
+        public static async Task<(long averageTime, long maxTime)> RunPerformanceTestAsync(Func<Task> testAction, int numberOfRuns)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            List<long> elapsedMillisecondsList = new List<long>();
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+
+            for (int i = 0; i < numberOfRuns; i++)
+            {
+                stopwatch.Restart();
+                await testAction.Invoke();
                 long elapsedMilliseconds = stopwatch.ElapsedMilliseconds;
                 elapsedMillisecondsList.Add(elapsedMilliseconds);
             }
